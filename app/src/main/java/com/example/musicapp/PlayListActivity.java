b1c4473b69f401d3305befcb3aad1c7fdef45a67
Kien <ntkien123@gmail.com>
2023-05-30 20:00:27 -0700

    commit update playList

--- app/src/main/java/com/example/musicapp/PlayListActivity.java
+++ app/src/main/java/com/example/musicapp/PlayListActivity.java
@@ -30,6 +30,7 @@ import com.google.gson.Gson;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class PlayListActivity extends AppCompatActivity {
@@ -50,12 +51,12 @@ public class PlayListActivity extends AppCompatActivity {
         getAnhXa();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         nameLPlayList = new ArrayList<>();
-        loadLists();
+
 
 
         adapter = new PlayListAdapter(PlayListActivity.this,R.layout.view_play_list, arrayList);
         listViewPlay1.setAdapter(adapter);
-
+        loadLists();
         Button btn_add = findViewById(R.id.addPlayList);
         btn_add.setOnClickListener(new AdapterView.OnClickListener(){
 
@@ -73,7 +74,7 @@ public class PlayListActivity extends AppCompatActivity {
         System.out.println("P"+position);
         System.out.println("A"+selectedItem);
         startActivity(new Intent(PlayListActivity.this,ListSongActivity.class)
-                .putExtra("NAMELIST","myFavorites").putExtra("NAMEMENU","EDM")
+                .putExtra("NAMELIST",selectedItem.getName()).putExtra("NAMEMENU","PlayList")
                 .putExtra("ly","PlayListActivity"));
         // Xử lý logic với selectedItem
     }
@@ -129,70 +130,72 @@ public class PlayListActivity extends AppCompatActivity {
         Gson gson = new Gson();
         Account restoredAccount = gson.fromJson(accountJson, Account.class);
         if(restoredAccount!=null){
+            for (String name :restoredAccount.getMyPlaylist()){
+                nameLPlayList.add(name);
+            }
 
         }
-        mDatabase.child("playList").addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                numChildren++;
-                String playlistKey = snapshot.getKey(); // Lấy mã của playlist
-                Map<String, Object> playlistData = (Map<String, Object>) snapshot.getValue();
-                String nameList = (String) playlistData.get("nameList"); // Lấy giá trị của thuộc tính "nameList"
-                System.out.println("Key: " + playlistKey + ", Name: " + nameList);
-                nameLPlayList.add(nameList);
-                // Kiểm tra xem đã lấy được tất cả dữ liệu playlists chưa
-                System.out.println("s"+nameLPlayList.size());
-                System.out.println("c"+ numChildren);
-                if (nameLPlayList.size() == numChildren) {
-                    System.out.println("Load");
-                    loadSongs(numChildren);
-                }
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                numChildren--;
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        loadSongs();
+//        mDatabase.child("playList").addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//                numChildren++;
+//                String playlistKey = snapshot.getKey(); // Lấy mã của playlist
+//                Map<String, Object> playlistData = (Map<String, Object>) snapshot.getValue();
+//                // Lấy giá trị của thuộc tính "nameList"
+//                System.out.println("s"+nameLPlayList.size());
+//                System.out.println("c"+ numChildren);
+//                if (nameLPlayList.size() == numChildren) {
+//                    System.out.println("Load");
+//                    loadSongs();
+//                }
+//            }
+//
+//            @Override
+//            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+//                numChildren--;
+//            }
+//
+//            @Override
+//            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
 
 
 
     }
-    private void loadSongs(int numChildren){
-        numSong = 0;
-
-        String i = nameLPlayList.get(numChildren-1);
-        System.out.println("name" + i);
-        ArrayList <Song> listSong = new ArrayList<>();
-        arrayList.add(new PlayList(i,numSong,"https://firebasestorage.googleapis.com/v0/b/musicpjandroid.appspot.com/o/img%2FRight%20Now%20Na%20Na%20Na%20Lyrics.jpg?alt=media&token=41c70495-7128-49da-b0e0-e9e6d8f16dcb",null));
-        adapter.notifyDataSetChanged();
-            mDatabase.child(i.trim()).addChildEventListener(new ChildEventListener() {
+    private void loadSongs(){
+        numChildren = 0;
+        for (int i = 0; i < nameLPlayList.size(); i++) {
+            numChildren++;
+            numSong = 0;
+            ArrayList <Song> listSong = new ArrayList<>();
+            arrayList.add(new PlayList(nameLPlayList.get(i),numSong,"https://firebasestorage.googleapis.com/v0/b/musicpjandroid.appspot.com/o/img%2FRight%20Now%20Na%20Na%20Na%20Lyrics.jpg?alt=media&token=41c70495-7128-49da-b0e0-e9e6d8f16dcb",null));
+            adapter.notifyDataSetChanged();
+            mDatabase.child(nameLPlayList.get(i).trim()).addChildEventListener(new ChildEventListener() {
                 @Override
                 public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                     Song songs=snapshot.getValue(Song.class);
                     System.out.println("song" +songs);
                     listSong.add(songs);
                     numSong++;
-                    arrayList.get(numChildren-1).setSize(numSong);
-                    arrayList.get(numChildren-1).setList(listSong);
+                    arrayList.get(numChildren-2).setSize(numSong);
+                    arrayList.get(numChildren-2).setList(listSong);
                     adapter.notifyDataSetChanged();
 
                 }
+
 
                 @Override
                 public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
@@ -214,6 +217,8 @@ public class PlayListActivity extends AppCompatActivity {
 
                 }
             });
+
+        };
 
     }
     public void getAnhXa(){
